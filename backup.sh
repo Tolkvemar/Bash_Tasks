9c53f2b47460fb2accdf175743ce57a32e077506
Alexandr <99329200+Tolkvemar@users.noreply.github.com>
2022-11-13 13:54:01 +0200

    One more key

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -arb --info=BACKUP,MISC2,NAME2,REMOVE  /home/source/ /home/backup --log-file=/home/backup.log 
+rsync -arbc --info=BACKUP,MISC2,NAME2,REMOVE  /home/source/ /home/backup --log-file=/home/backup.log 
